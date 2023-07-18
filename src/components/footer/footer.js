dd8440b7f5cd6b959d2b18f39cafb15fb85f55f8
Miss-Melissa <melissahh@live.se>
2023-07-18 20:33:58 +0200

    started with footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import '../../styles/footer/footer.css' ;
+import '../../styles/footer/footer.css';
 
 export default function Footer() {
   return (
     <div className='footer'>
-        <p>Footer</p>
 
+      <div className='a'>
+
+        <div>
+          <div><h3>Rubrik 1</h3></div>
+          <div>w</div>
+          <div>w</div>
+        </div>
+
+        <div>
+          <div><h3>Rubrik 2</h3></div>
+          <div>w</div>
+          <div>w</div>
+        </div>
+
+        <div>
+          <div><h3>Rubrik 3</h3></div>
+          <div>w</div>
+          <div>w</div>
+        </div>
+
+      </div>
+      
     </div>
+
   );
 }
