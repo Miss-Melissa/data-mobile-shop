f0b0df9812622cafc5b91997e71003f1b81a8b71
Miss-Melissa <melissahh@live.se>
2023-07-20 22:29:48 +0200

    footer added

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../../styles/home/home.css';
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <h1>Home</h1>
+
+      <h1>Home</h1>
+
+      <h1>Home</h1>
+
+      <h1>Home</h1>
+
+      <h1>Home</h1>
+
     </div>
   );
 }
